f11294600221babcad28528d90c81b4910416999
Andrew Clarke <andrewneilclarke@gmail.com>
2021-07-18 10:26:52 +0200

    updated script.js

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
+// functions
 const openModal = () => (modal.style.display = 'block');
+const closeModal = () => (modal.style.display = 'none');
+const outsideClick = (e) => {
+  if (e.target == modal) {
+    closeModal();
+  }
+};
+
+// DOM elements
 const modal = document.querySelector('.modal');
 const loginBtn = document.querySelector('.login-btn');
 const closeBtn = document.querySelector('.close');
 
+// event listeners
 loginBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
-
-// functions
-
-// function openModal() {
-//   modal.style.display = 'block';
-// }
-
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-function outsideClick(e) {
-  if (e.target == modal) {
-    closeModal();
-  }
-}
-// const openModal = () => (modal.style.display = 'block');
-// const closeModal = () => (modal.style.display = 'none');
